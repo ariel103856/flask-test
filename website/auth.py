f7789c882904f063f1f3337cffb860870419fa3d
ariel103856 <ariel103856@gmail.com>
2023-09-22 12:04:32 +0300

    Second commit

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', header="This is Login page")
+    data = request.form
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return render_template('login.html', header="This is Login page")
+    return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html', header="This is Sign Up page")
+    username = request.form.get('username')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    if len(username) < 2:
+        flash('Username must be longer then 1 characters')
+    elif len(password1) < 8:
+        flash('Password must be at least 8 characters')
+    elif password1 != password2:
+        flash('Passwords don\'t match')
+    else:
+        pass
+    return render_template('signup.html')
